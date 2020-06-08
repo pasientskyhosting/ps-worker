ff4a7221d1996a4479d91f36ea13964e5c49e97c
Chad Jones <cj@patientsky.com>
2020-06-08 21:31:52 +0200

    php 7.4

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,11 +16,11 @@ if [ ! -z "$NEW_RELIC_LICENSE_KEY" ]; then
     nrsysmond-config --set license_key=${NEW_RELIC_LICENSE_KEY} || exit 1
     echo -e "\n[program:nrsysmond]\ncommand=nrsysmond -c /etc/newrelic/nrsysmond.cfg -l /dev/stdout -f\nautostart=true\nautorestart=true\npriority=0\nstdout_events_enabled=true\nstderr_events_enabled=true\nstdout_logfile=/dev/stdout\nstdout_logfile_maxbytes=0\nstderr_logfile=/dev/stderr\nstderr_logfile_maxbytes=0" >> /etc/supervisord.conf
 else
-    if [ -f /etc/php/7.1/fpm/conf.d/20-newrelic.ini ]; then
-        rm -rf /etc/php/7.1/fpm/conf.d/20-newrelic.ini
+    if [ -f /etc/php/7.4/fpm/conf.d/20-newrelic.ini ]; then
+        rm -rf /etc/php/7.4/fpm/conf.d/20-newrelic.ini
     fi
-    if [ -f /etc/php/7.1/cli/conf.d/20-newrelic.ini ]; then
-        rm -rf /etc/php/7.1/cli/conf.d/20-newrelic.ini
+    if [ -f /etc/php/7.4/cli/conf.d/20-newrelic.ini ]; then
+        rm -rf /etc/php/7.4/cli/conf.d/20-newrelic.ini
     fi
     /etc/init.d/newrelic-daemon stop
 fi
